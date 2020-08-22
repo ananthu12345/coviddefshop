ad56fd4bc2f052147ef923b407ce86337f7f8438
JithinAji <jithin_aji@outlook.com>
2020-08-22 12:30:13 +0530

    changed colour and theme

---
+++ tailwindcss-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    maxHeight: {
+      +       '0': '0',
+      +       '1/4': '25%',
+      +       '1/2': '50%',
+      +       '3/4': '75%',
+      +       '9/10': '80%',
+      +       'full': '100%',
+            }
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
